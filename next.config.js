f89da02f03289711b4d797725fbff1e00df2986c
Chukkrit Visitsaktavorn <chukkrit.juk@gmail.com>
2021-10-22 23:49:50 +0700

    chore: update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  poweredByHeader: false,
+  experimental: {
+    esmExternals: true,
+    // swcMinify: true,
+    // swcLoader: true,
+  },
+  pageExtensions: ["ts", "tsx", "mdx"],
+};
