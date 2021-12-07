0ebe3e448f5b37dc2cae5a18f718a88522192d71
Chukkrit Visitsaktavorn <chukkrit.juk@gmail.com>
2021-12-08 01:04:00 +0700

    fix: disable unused imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+// import path from 'path'
+// import { fileURLToPath } from 'url'
+// const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const ContentSecurityPolicy = `
   default-src 'self' data:;
