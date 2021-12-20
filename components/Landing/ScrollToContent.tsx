0edaa917a29e798304eacbbdd62db2c210fef352
Chukkrit Visitsaktavorn <chukkrit.juk@gmail.com>
2021-12-21 02:45:21 +0700

    fix: hover scale

--- components/Landing/ScrollToContent.tsx
+++ components/Landing/ScrollToContent.tsx
@@ -3,9 +3,9 @@ import { ArrowDownIcon } from '@heroicons/react/solid'
 const ScrollToContent = (): JSX.Element => {
   return (
     <div className="absolute flex justify-center w-full left-0 -top-24">
-      <div className="p-2 border-2 border-dashed group transform-gpu transition-all border-primary rounded-[100%] w-40 h-40">
+      <div className="p-2 border-2 border-dashed group transform-gpu transition-all border-primary rounded-[100%] hover:border hover:scale-125 w-40 h-40">
         <a href="#learnmore">
-          <div className="flex flex-col items-center justify-center h-full pt-4 space-y-2 text-center text-white bg-primary group-hover:scale-125 rounded-[100%] p-2">
+          <div className="flex flex-col items-center justify-center h-full pt-4 space-y-2 text-center text-white bg-primary rounded-[100%] p-2">
             <p className="text-sm sm:text-base font-body">
               รู้จักกับ
               <br /> โพลิซี ไดอะล็อก
