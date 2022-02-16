0f2d57689d1d9d91a8cc1b3d55ef5cc7a5ecc4ff
Chukkrit Visitsaktavorn <chukkrit.juk@gmail.com>
2022-02-17 00:34:51 +0700

    fix: policy mobile position

--- components/Project/RelatedPolicy.tsx
+++ components/Project/RelatedPolicy.tsx
@@ -14,7 +14,7 @@ const RelatedPolicy = ({ policies = [] }: Props) => {
           <h2 className="text-4xl font-bold">ข้อเสนอเชิงนโยบายที่เกี่ยวข้อง</h2>
           <p className="text-base font-body">จากการสังเคราะห์ข้อมูลที่ได้มาจากกระบวนการที่ใช้วิธีการอันหลากหลาย จึงเกิดเป็นข้อเสนอเชิงนโยบายขึ้น</p>
         </div>
-        <ul className="flex w-full space-x-6 overflow-x-auto justify-center md:space-x-12 flex-nowrap">
+        <ul className={`flex w-full space-x-6 overflow-x-auto ${policies.length === 1 && 'justify-center'} md:justify-center md:space-x-12 flex-nowrap`}>
           {policies.map((policy) => (
             <li key={policy.id} className={`shrink-0 flex flex-col items-center justify-between w-64 p-6 ${policy.color} text-white h-72 text-center`}>
               <p className="mt-12 font-bold whitespace-normal">{policy.text}</p>
