27aef35c3839d90edbad78350925e72ae90ab32d
Chukkrit Visitsaktavorn <chukkrit.juk@gmail.com>
2022-03-02 20:32:55 +0700

    refactor: reduce padding size on mobile

--- pages/article/index.tsx
+++ pages/article/index.tsx
@@ -118,7 +118,7 @@ const ArticlesPage = ({ articlesConnection, articles, banners, tags }: Props) =>
                 {currentArticles.map(({ id, title, url, coverImage, description }) => (
                   <li key={id} className="flex flex-col w-full overflow-hidden transition ease-in bg-white">
                     <img className="object-top object-cover w-full h-[240px] flex-shrink-0 bg-[#2b2b2b]" loading="eager" src={coverImage.url} alt={title} />
-                    <div className="flex flex-col h-full px-12 py-8">
+                    <div className="flex flex-col h-full p-6 sm:px-12 sm:py-8">
                       <div className="mb-12 space-y-4">
                         <h3 className="text-lg font-bold lg:text-xl line-clamp-2">{title}</h3>
                         <p className="text-sm font-body leading-relaxed line-clamp-4 text-[#707070]">{description}</p>
